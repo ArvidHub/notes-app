a437ae8c418f12db11e517f39da9edd2c3753c54
Arvid <arvid.e.westberg@gmail.com>
2025-01-27 20:48:08 +0000

    övning 2: använd chalk i terminalen

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-const notes = require('./notes.js');
-console.log(notes());
+// const getNotes = require('./notes.js');
+// const msg = getNotes();
+// console.log(msg);
+
+import chalk from 'chalk';
+
+console.log(chalk.green.bold.inverse('Success!'));
