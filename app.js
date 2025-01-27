aa179ce0b0344b48fb66726324f7c1b6acbd1526
Arvid <arvid.e.westberg@gmail.com>
2025-01-27 20:46:36 +0000

    övning 2: använd chalk

--- app.js
+++ app.js
@@ -1,5 +1,2 @@
-// challange: append a message to notes.txt
-
-const fs = require('fs')
-
-fs.appendFileSync('notes.txt', '\nbla bla text här')
+const notes = require('./notes.js');
+console.log(notes());
---
+++ notes.js
@@ -0,0 +1,5 @@
+const getNotes = () => {
+    return 'lorem ipsum dolor sit amet consectetur adipisicing elit...';
+}
+
+module.exports = getNotes;
