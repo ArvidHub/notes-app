f44a739bae14dd2a88ac45c30744230a9d0e05bb
Arvid <arvid.e.westberg@gmail.com>
2025-02-01 15:46:39 +0000

    weather-app: Add Handlebars as view engine and create views for index and help pages; remove old HTML files

--- node/web-server/src/app.js
+++ node/web-server/src/app.js
@@ -3,11 +3,29 @@ import express from 'express';
 
 const app = express();
 const publicDirectoryPath = path.join(dirname('./'), '../public');
+const viewsPath = path.join(dirname('./'), '../views');
 
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
 app.use(express.static(publicDirectoryPath));
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Andrew Mead'
+    });
+});
 
 app.get('/weather', (req, res) => {
     res.send('Weather Page');
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page',
+        helpText: 'This is some helpful text.',
+        name: 'Andrew Mead'
+    });
 });
 
 app.listen(3000, () => {
