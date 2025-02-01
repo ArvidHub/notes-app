66a1f21497cbd6761e47ae72a5f6352a6bde2c1f
Arvid <arvid.e.westberg@gmail.com>
2025-02-01 10:36:19 +0000

    weather-app del 2: printar long/lat från t.ex "stockholm"

--- node/weather-app/app.js
+++ node/weather-app/app.js
@@ -1,13 +1,15 @@
 import request from 'request';
 
-const url = 'http://api.weatherstack.com/current?access_key=3d6e3a4fdac86dbb7c8091076511d5ac&query=37.8267,-122.4233';
+const url = `https://api.mapbox.com/search/geocode/v6/forward?q=Stockholm&access_token=pk.eyJ1Ijoia2xhc29obHNzb24iLCJhIjoiY202bTB5cTl0MGcxajJsczcxaGR6c21qcSJ9.SrjYU1xXw_b30Ogev5_iOw&limit=1`;
 
 request({ url: url, json: true }, (error, response) => {
     if (error) {
-        console.log('Unable to connect to weather service!');
-    } else if (response.body.error) {
-        console.log('Unable to find location');
+        console.log('Unable to connect to location services!');
+    } else if (response.body.features.length === 0) {
+        console.log('Unable to find location. Try another search.');
     } else {
-        console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out.');
+        const longitude = response.body.features[0].geometry.coordinates[0];
+        const latitude = response.body.features[0].geometry.coordinates[1];
+        console.log(latitude, longitude);
     }
 });
