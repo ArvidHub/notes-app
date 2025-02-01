e6f98f6eb646574a579e21702a8c5973ba29dd79
Arvid <arvid.e.westberg@gmail.com>
2025-02-01 12:36:33 +0000

    weather-app: användare kan ge input (adress) och få ut väder information

--- node/weather-app/app.js
+++ node/weather-app/app.js
@@ -1,15 +1,17 @@
-import request from 'request';
+import { geocode } from "./utils/geocode.js";
+import { forecast } from "./utils/forecast.js";
 
-const url = `https://api.mapbox.com/search/geocode/v6/forward?q=Stockholm&access_token=pk.eyJ1Ijoia2xhc29obHNzb24iLCJhIjoiY202bTB5cTl0MGcxajJsczcxaGR6c21qcSJ9.SrjYU1xXw_b30Ogev5_iOw&limit=1`;
+const locationInput = process.argv[2];
 
-request({ url: url, json: true }, (error, response) => {
+geocode(locationInput, (error, data) => {
     if (error) {
-        console.log('Unable to connect to location services!');
-    } else if (response.body.features.length === 0) {
-        console.log('Unable to find location. Try another search.');
-    } else {
-        const longitude = response.body.features[0].geometry.coordinates[0];
-        const latitude = response.body.features[0].geometry.coordinates[1];
-        console.log(latitude, longitude);
+        return console.log('Error', error);
     }
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if (error) {
+            return console.log('Error', error);
+        }
+        console.log(data.location);
+        console.log(forecastData);
+    });
 });
