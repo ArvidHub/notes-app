15cc4241069ec0ce6679b5b2a81299e078f4f2a2
Arvid <arvid.e.westberg@gmail.com>
2025-02-02 11:07:10 +0000

    weather-app: Implement Handlebars templates for header, footer, and views; update app configuration

--- node/web-server/src/app.js
+++ node/web-server/src/app.js
@@ -1,14 +1,17 @@
 import path, { dirname } from 'path';
 import express from 'express';
+import hbs from 'hbs';
 
 const app = express();
 
 // define paths for Express config
 const publicDirectoryPath = path.join(dirname('./'), '../public');
-const viewsPath = path.join(dirname('./'), '../views');
+const viewsPath = path.join(dirname('./'), '../templates/views');
+const partialsPath = path.join(dirname('./'), '../templates/partials');
 
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 // setup static directory to serve
 app.use(express.static(publicDirectoryPath));
@@ -16,22 +19,46 @@ app.use(express.static(publicDirectoryPath));
 app.get('/', (req, res) => {
     res.render('index', {
         title: 'Weather App',
-        name: 'Andrew Mead'
+        name: 'Arvid Westberg'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Arvid Westberg'
     });
 });
 
 app.get('/weather', (req, res) => {
     res.send('Weather Page');
+
 });
 
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help Page',
         helpText: 'This is some helpful text.',
-        name: 'Andrew Mead'
+        name: 'Arvid Westberg'
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Arvid Westberg',
+        errorMessage: 'Help article not found.'
+    });
+});
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Arvid Westberg',
+        errorMessage: 'Page not found.'
+    });
+});
+
+app.listen(3049, () => {
+    console.log('Server is running on http://localhost:3049');
 });
