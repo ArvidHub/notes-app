00c3e27b5d5ccfcef63fcf6583c8fd709b9e89c7
Arvid Westberg <arvid.e.westberg@gmail.com>
2025-02-04 20:02:20 +0100

    task-manager: CRUD operations

--- node/task-manager/mongodb.js
+++ node/task-manager/mongodb.js
@@ -1,25 +1,50 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient, ObjectId } from 'mongodb'
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
 const client = new MongoClient(connectionURL)
 
-const connect = async () => {
-    try {
-        await client.connect()
-        
-        const db = client.db(databaseName)
-        // You can perform database operations here
-        db.collection('users').insertOne({
-            name: 'John',
-            age: 30
-        })
+client.connect().then(() => {
+    const db = client.db(databaseName)
+    db.collection('tasks').updateMany({ // false to true
+        completed: false
+    }, {
+        $set: {
+            completed: true
+        }
+    }).then((result) => {
+        console.log(result.modifiedCount)
+    }).catch((error) => {
+        console.log(error)
+    })
 
-        
-    } catch (error) {
-        console.log('Unable to connect to database!', error)
-    }
-}
+    // db.collection('tasks').findOne({ // find one task
+    //     _id: new ObjectId('5f2e2e5b7a1f8b0e5c9a4d0b')
+    // }).then((task) => {
+    //     console.log(task)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
 
-connect()
+    // create a new task
+    // db.collection('tasks').insertOne({
+    //     description: 'Task 1',
+    //     completed: false
+    // }).then((result) => {
+    //     console.log(result.ops)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+
+    // db.collection('tasks').deleteOne({ // delete one task
+    //     description: 'Task 1'
+    // }).then((result) => {
+    //     console.log(result.deletedCount)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+
+}).catch((error) => {
+    console.log('Error connecting to database:', error)
+})
