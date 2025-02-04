5f1bd3b3176d95ba425145cec861dd6fc22422d2
Arvid Westberg <arvid.e.westberg@gmail.com>
2025-02-04 13:35:55 +0100

    weather-app: Add weather search form and update styles; implement client-side JavaScript for fetching weather data

--- node/web-server/public/js/app.js
+++ node/web-server/public/js/app.js
@@ -1,2 +1,23 @@
 
-console.log('app.js is running!');
+console.log('Client side javascript file is loaded!');
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const displayForecast = document.querySelector('#forecast');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    displayForecast.textContent = 'Loading...';
+    const adress = search.value;
+
+    fetch(`http://localhost:3000/weather?adress=${adress}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                displayForecast.textContent = data.error;
+            } else {
+                displayForecast.textContent = `${data.location}: ${data.forecast}`;
+            }
+        });
+    });
+});
