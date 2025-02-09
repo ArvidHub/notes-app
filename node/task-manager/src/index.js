7d8ea9e9cf5ca373f1bd40c3703295bf80bbb7ee
Arvid Westberg <arvid.e.westberg@gmail.com>
2025-02-09 10:46:07 +0100

    Task-app: restructure task and user models, and implement routers for improved organization

--- node/task-manager/src/index.js
+++ node/task-manager/src/index.js
@@ -1,73 +1,16 @@
 import express from 'express';
-import User from '../models/user.js';
-import Task from '../models/task.js';
 import './db/mongoose.js';
+import taskRouter from './routers/task.js';
+import userRouter from './routers/user.js';
 
 const app = express();
 
 app.use(express.json());
+app.use(taskRouter);
+app.use(userRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello World');
-});
-
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
-        res.status(200).send(users)
-    }).catch((e) => {
-        res.status(500).send(e)
-    })
-})
-
-app.get('/users/:id', (req, res) => {
-    const _id = req.params.id;
-    User.findById(_id).then((user) => {
-        console.log(user);
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.status(200).send(user);
-    }).catch((e) => {
-        res.status(500).send();
-    });
-});
-
-app.post('/users', (req, res) => {
-    const user = new User(req.body);
-    user.save().then(() => {
-        res.status(201).send(user)
-    }).catch((error) => {
-        res.status(400).send(error)
-    });
-});
-
-app.get('/tasks', (req, res) => { 
-    Task.find({}).then((tasks) => {
-        res.status(200).send(tasks)
-    }).catch((e) => {
-        res.status(500).send(e)
-    })
-})
-
-app.get('/tasks/:id', (req, res) => {
-    const _id = req.params.id;
-    Task.findById(_id).then((task) => {
-        if (!task) {
-            return res.status(404).send()
-        }
-        res.status(200).send(task)
-    }).catch((e) => {
-        res.status(500).send();
-    });
-});
-
-app.post('/tasks', (req, res) => {
-    const task = new Task(req.body);
-    task.save().then(() => {
-        res.status(201).send(task)
-    }).catch((error) => {
-        res.status(400).send(error)
-    });
 });
 
 app.listen(3000, () => {
