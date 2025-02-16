4dcb43bdee672533fcaeb7c8cb5b69de641e1e4c
Arvid Westberg <arvid.e.westberg@gmail.com>
2025-02-16 17:55:00 +0100

    chat-app: using socket.io to displaying and sending messages in real time aswell  as location data

---
+++ node/chat-app/src/index.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import { createServer } from 'http'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+import { Server } from 'socket.io'
+import { Filter } from 'bad-words'
+
+const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+app.use(express.static(join(__dirname, '../public')))
+
+io.on('connection', (socket) => {
+  console.log('New websocket connection')
+
+  socket.on('sendMessage', (message, callback) => {
+    const filter = new Filter()
+
+    if (filter.isProfane(message)) {
+      return callback('Profanity is not allowed')
+    }
+    callback()
+    io.emit('message', message)
+  })
+
+  socket.on('sendLocation', ({ lat, long }, callback) => {
+    io.emit('locationMessage', `https://google.com/maps?q=${lat},${long}`)
+    callback()
+  })
+})
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000')
+})
